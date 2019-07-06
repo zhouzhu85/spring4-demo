f9a97e224d3de0a65597b6f424bb76d826a9fc34
zhouzhu85 <734837014@qq.com>
2019-07-06 22:57:43 +0800

    spring aop配置bean

--- src/main/java/com/spring4/knightDemo/BraveKnight.java
+++ src/main/java/com/spring4/knightDemo/BraveKnight.java
@@ -4,11 +4,20 @@ public class BraveKnight implements Knight{
 
     private Quest quest;
 
+    private Minstrel minstrel;
+
     public BraveKnight(Quest quest){
         this.quest=quest;
     }
 
+    public BraveKnight(Quest quest,Minstrel minstrel){
+        this.quest=quest;
+        this.minstrel=minstrel;
+    }
+
     public void embarkOnQuest() {
+        minstrel.singBeforeQuest();
         quest.embark();
+        minstrel.singAfterQuest();
     }
 }
